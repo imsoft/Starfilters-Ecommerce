e46cd245df133436eb522d5012d7bb12a3171f15
Brandon Uriel Garcia Ramos <weareimsoft@gmail.com>
2025-11-24 18:55:28 -0600

    feat: cambiar adapter de Node.js a Vercel para despliegue en Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,14 @@
 
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://starfilters.com',
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
 
   i18n: {
     defaultLocale: 'es',
